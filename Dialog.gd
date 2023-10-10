b8a5b1f72a736dbeb65d2087ca9cc59258c4fb93
jackokring <jackokring@gmail.com>
2023-10-10 21:16:24 +0000

    send signal

--- Dialog.gd
+++ Dialog.gd
@@ -1,5 +1,7 @@
 extends PanelContainer
 
+signal ok
+
 func _on_pause_button_pressed():
 	get_tree().paused = true
 	show()
@@ -9,18 +11,12 @@ func _on_pause_popup_cancel_pressed():
 	get_tree().paused = false
 
 func _on_pause_popup_ok_pressed():
-	ok()
+	# send data related to doing action
+	emit_signal("ok", null)
 	_on_pause_popup_cancel_pressed()
 
-func ok():
-	pass
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		if is_visible_in_tree():
 			_on_pause_popup_cancel_pressed()
@@ -29,5 +25,3 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		if is_visible_in_tree():
 			_on_pause_popup_ok_pressed()
-
-
