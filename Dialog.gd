23f71a48ae329cc1c613ab6c4bc788d64c2b9e0c
jackokring <jackokring@gmail.com>
2023-10-11 13:55:12 +0000

    hold escape exit

--- Dialog.gd
+++ Dialog.gd
@@ -1,6 +1,11 @@
 extends PanelContainer
 
 signal ok
+onready var exit_timer := $Timer
+
+func _ready():
+	# default to shown
+	_on_pause_button_pressed()
 
 func _on_pause_button_pressed():
 	get_tree().paused = true
@@ -15,13 +20,22 @@ func _on_pause_popup_ok_pressed():
 	emit_signal("ok", null)
 	_on_pause_popup_cancel_pressed()
 
+func _on_long_pause():
+	if Input.is_action_pressed("ui_cancel"):
+		# held
+		get_tree().root.propagate_notification(NOTIFICATION_WM_QUIT_REQUEST)
+		get_tree().quit()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
+		exit_timer.start(exit_timer.wait_time)
 		if is_visible_in_tree():
 			_on_pause_popup_cancel_pressed()
 		else:
 			_on_pause_button_pressed()
+	if Input.is_action_just_released("ui_cancel"):
+		exit_timer.stop()
 	if Input.is_action_just_pressed("ui_accept"):
 		if is_visible_in_tree():
 			_on_pause_popup_ok_pressed()
